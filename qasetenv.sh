61ff2f1c658212119a55d5d543f2f962fc97b36d
Huseyin Kaya <88791134+kayahuseyin64@users.noreply.github.com>
2022-03-19 17:20:41 -0500

    added qa setenv

---
+++ qasetenv.sh
@@ -0,0 +1,11 @@
+DIR=$(pwd)
+cat << EOF > "$DIR/backend.tf"
+terraform {
+  backend "gcs" {
+    bucket  = "`gsutil ls  | grep "backend-" | awk -F "/" '{print $3}'`"
+    prefix  = "application_deployment/qa"
+    credentials = "service-account.json"
+  }
+}
+EOF
+cat "$DIR/backend.tf"
