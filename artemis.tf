0321018e5f71cb8f855718846088921ac96f3420
Huseyin Kaya <88791134+kayahuseyin64@users.noreply.github.com>
2022-03-19 17:34:43 -0500

    added the namespace

--- artemis.tf
+++ artemis.tf
@@ -25,7 +25,7 @@ ingress:
     acme.cert-manager.io/http01-edit-in-place: "true"
     kubernetes.io/ingress.class: nginx
   hosts:
-  - host: "artemis-qa.${var.google_domain_name}"
+  - host: "artemis-${var.namespace}.${var.google_domain_name}"
     paths: 
     - path: /
   tls: 
