04d09c1b96a9b683be5a3b5bb18d0b4563bb61cb
Huseyin Kaya <88791134+kayahuseyin64@users.noreply.github.com>
2022-03-21 15:33:23 -0500

    changed setenv

--- setenv.sh
+++ setenv.sh
@@ -1,9 +1,11 @@
+NAMESPACE=`grep namespace $1| awk -F "=" '{print $2}' | tr -d '"' | tr -d ' '`
+
 DIR=$(pwd)
 cat << EOF > "$DIR/backend.tf"
 terraform {
   backend "gcs" {
     bucket  = "`gsutil ls  | grep "backend-" | awk -F "/" '{print $3}'`"
-    prefix  = "application_deployment/dev"
+    prefix  = "application_deployment/$NAMESPACE"
     credentials = "service-account.json"
   }
 }
